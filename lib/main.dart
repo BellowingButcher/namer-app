cc192aa9b67ce794ed29fbbd3ab841e10c0abbaa
Mason Williams <bellowingbutcher@gmail.com>
2023-01-26 09:17:40 -0500

    app is centered horizontaliy

--- lib/main.dart
+++ lib/main.dart
@@ -41,18 +41,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('An AWESOME computerized RANDOM idea:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('An AWESOME computerized RANDOM idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            )
+          ],
+        ),
       ),
     );
   }
